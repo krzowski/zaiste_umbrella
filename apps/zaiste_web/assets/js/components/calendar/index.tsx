25a21888bab4f493260a4fb70bb105bf4ab94fe1
Krzysztof Ołdakowski <krz.oldakowski@gmail.com>
2020-11-11 19:55:03 +0100

    Add date-fns for js date manipulations

--- apps/zaiste_web/assets/js/components/calendar/index.tsx
+++ apps/zaiste_web/assets/js/components/calendar/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
+import { format } from 'date-fns'
 
 import Spinner from '../shared/Spinner'
 import ErrorMessage from '../shared/ErrorMessage'
@@ -8,7 +9,7 @@ import EventsCalendar from './EventsCalendar'
 
 
 const Calendar: React.FC<RouteComponentProps> = () => {
-  const [calendarDate, setCalendarDate] = React.useState<string>(new Date().toISOString().slice(0,10))
+  const [calendarDate, setCalendarDate] = React.useState<string>(format(new Date(), 'yyyy-MM-dd'))
 
   const { isLoading, data, errorMessage } = useFetch('/calendar_events/month_events', { date: calendarDate })
 
