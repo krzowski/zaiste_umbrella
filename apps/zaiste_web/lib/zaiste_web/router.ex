571f4477b4a93a4ee2d5c3cd0929fc811e42ae64
Krzysztof Ołdakowski <krz.oldakowski@gmail.com>
2021-06-16 20:48:18 +0200

    Enable LiveDashboard in routes

--- apps/zaiste_web/lib/zaiste_web/router.ex
+++ apps/zaiste_web/lib/zaiste_web/router.ex
@@ -9,13 +9,29 @@ defmodule ZaisteWeb.Router do
     plug :put_secure_browser_headers
   end
 
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: ZaisteWeb.Telemetry
+    end
+  end
+
+
   pipeline :api do
     plug :accepts, ["json"]
 
     scope "/calendar_events" do
       get "/month_events", ZaisteWeb.CalendarEventController, :month_events
     end
-
     resources "/calendar_events", ZaisteWeb.CalendarEventController, except: [:new, :edit]
 
     resources "/expenses", ZaisteWeb.ExpenseController, except: [:new, :edit]
@@ -32,20 +48,4 @@ defmodule ZaisteWeb.Router do
   # scope "/api", ZaisteWeb do
   #   pipe_through :api
   # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: ZaisteWeb.Telemetry
-    end
-  end
 end
